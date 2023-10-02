55b50d364b9833b7d01410f8539e398c7caaa36d
Ashutosh <ashutoshdev16@gmail.com>
2023-10-02 12:22:56 +0530

    learnt memeber functions

--- ClassAndObject/src/Main.java
+++ ClassAndObject/src/Main.java
@@ -2,6 +2,10 @@ class Student{
     String name;
     int rollno;
     int marks;
+
+    void study(String subject){
+        System.out.println("The student is studying "+subject);
+    }
 }
 public class Main {
     public static void main(String[] args) {
@@ -10,7 +14,7 @@ public class Main {
         ashutosh.name = "Ashutosh Pandey";
         ashutosh.rollno = 1;
         ashutosh.marks = 100;
-
+        ashutosh.study("DSA & LLD");
         System.out.println(ashutosh.name+" "+ashutosh.marks+" "+ashutosh.rollno);
     }
 }
