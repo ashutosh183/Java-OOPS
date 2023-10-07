876012cbb542a46df666e919d47cade9434ea886
Ashutosh <ashutoshdev16@gmail.com>
2023-10-07 20:40:16 +0530

    learnt multilevel inheritance

--- Inheritance/src/Main.java
+++ Inheritance/src/Main.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 class Car{
     private String plateNo;
     private String make;
@@ -48,6 +50,27 @@ class PetrolCar extends Car{
         this.seats = seats;
     }
 }
+
+class SportsCar extends PetrolCar{
+    String carType;
+    String horsePower;
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
+
+    public String getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(String horsePower) {
+        this.horsePower = horsePower;
+    }
+}
 public class Main {
     public static void main(String[] args) {
         System.out.println("Learning Inheritance");
@@ -58,7 +81,21 @@ public class Main {
         pc.setCompany("Audi");
         pc.setMake("RS 8");
         pc.setPlateNo("TN 24 AK 0001");
-
+        System.out.println("Single Level Inheritance");
         System.out.println(pc.getFuelType()+" "+pc.getCompany()+" "+pc.getMake()+" "+pc.getSeats()+" "+pc.getPlateNo());
+
+
+        SportsCar sc = new SportsCar();
+        sc.setCarType("Sports");
+        sc.setHorsePower("860HP");
+
+        sc.setCompany("Buggati");
+        sc.setSeats(2);
+        sc.setMake("Chiron");
+        sc.setFuelType("Petrol");
+        sc.setPlateNo("ASHUTOSH RUPALI");
+
+        System.out.println("MULTILEVEL INHERITANCE");
+        System.out.println(sc.getCarType()+" "+sc.getCompany()+" "+sc.getFuelType()+" "+sc.getHorsePower()+" "+sc.getMake()+" "+sc.getPlateNo()+" "+sc.getSeats());
     }
 }
